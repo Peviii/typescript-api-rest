2307814b05414f29cc5c041cb7830ef687c446a9
Pedro Gulinelli <90875987+Peviii@users.noreply.github.com>
2023-02-18 12:17:07 -0300

    Update movie.model.ts

--- src/models/movie.model.ts
+++ src/models/movie.model.ts
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
+import { studioType } from './studio.model';
 
 export interface movieType extends mongoose.Document {
     name: string;
     year: number;
     gender: string;
     duration: string;
-    studio: string;
+    studio: studioType['_id'];
 }
 
 const movieSchema = new mongoose.Schema({
@@ -13,7 +14,7 @@ const movieSchema = new mongoose.Schema({
     year: { type: Number, required: true },
     gender: { type: String, required: true },
     duration: { type: String, required: true },
-    studio: { type: String, required: true }
+    studio: { type: mongoose.Schema.Types.ObjectId, ref: 'studio', required: true }
 }, { timestamps: true });
 
 const Movie = mongoose.model<movieType>('Movie', movieSchema);
